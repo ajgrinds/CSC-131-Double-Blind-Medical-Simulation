e7c23a65f0a4e09423dc8e729955d0eb5aac7839
khalidh7 <kabouelrous@gmail.com>
2023-03-05 13:27:15 -0800

    setting up bavaria

--- src/Bavaria/Bavaria.js
+++ src/Bavaria/Bavaria.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import {Routes, Route, Navigate} from "react-router-dom";
 
-const Bavaria = () => {
-  return (
-    <div>Bavaria</div>
-  )
+import TopBar from "./scenes/topbar";
+import BavariaSidebar from "./scenes/sidebar";
+import BavariaDashboard from "./scenes/dashboard";
+
+
+function FDA() {
+
+  const [theme, colorMode] = useMode();
+
+  return ( 
+  <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <div className="app">
+
+       <BavariaSidebar/>
+
+        <main className="content">
+          <TopBar/>
+          
+          <Routes>
+            <Route path="/" element={<BavariaDashboard/>}/>
+            
+
+          </Routes>
+  
+
+        </main>
+
+      </div>
+
+    </ThemeProvider>
+  </ColorModeContext.Provider>
+    
+  );
 }
 
-export default Bavaria
+export default FDA;
