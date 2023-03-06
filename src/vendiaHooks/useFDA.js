82a98938c99a0e556841b74397e70b55cafd568f
Raheem Abu Humra <124087748+raheem6@users.noreply.github.com>
2023-03-06 09:20:07 -0800

    Update useFDA.js
    Signed-off-by: Raheem Abu Humra <124087748+raheem6@users.noreply.github.com>

--- src/vendiaHooks/useFDA.js
+++ src/vendiaHooks/useFDA.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { createVendiaClient } from "@vendia/client";
+
+const client = createVendiaClient({
+  apiUrl: `https://ncd14u7pif.execute-api.us-west-2.amazonaws.com/graphql/`,
+  websocketUrl: `wss://3npxswgmxd.execute-api.us-west-2.amazonaws.com/graphql`,
+  apiKey: `624LRx7BELcNHN3UPpmgrRn7wHQ1XzHPqVUKc9objmPN`, // <---- API key
+});
+
+const { entities } = client;
 
 const useFDA = () => {
-  return (
-    <div>useFDA</div>
-  )
-}
+  return { entities };
+};
 
-export default useFDA
+export default useFDA;
