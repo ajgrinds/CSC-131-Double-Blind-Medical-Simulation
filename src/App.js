e3e1479e22c16abffbf6edbb2bdec82ff1cfb22e
Laura Kay <laurakay@Lauras-MacBook-Pro.local>
2023-03-07 14:34:18 -0800

    forgot routing in app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Bavaria from "./Bavaria/Bavaria";
 import FDA from "./FDA/FDA";
 import FDAPatient from "./FDA/scenes/patient"
 import FDADashboard from "./FDA/scenes/dashboard";
+import AddPatient from "./JaneHopkins/scenes/addpatient";
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
       <Route path="/*" element={<Doctor/>}>
         
         <Route path="patient" element={<Patient/>}/>
+        <Route path="addpatient" element={<AddPatient/>} />
         <Route path="patient/:id" element={<PatientDetails/>}/>
 
       </Route>
