8c4680e48d3d662612e02715ebb91f6ccd9260e0
Luke Simoni <luke.simoni2001@gmail.com>
2023-03-06 16:27:26 -0800

    Added firebase configuration page and a jsx file for login page

---
+++ src/scenes/login/login-Index.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signOut, onAuthStateChanged } from "firebase/auth";
+import { auth } from "firebase/auth";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/system";
+
+const Login = () =>  {
+
+    const[registerEmail, setRegisterEmail] = useState("");
+    const[registerPassword, setRegisterPassword] = useState("");
+    const[loginEmail, setLoginEmail] = useState("");
+    const[loginPassword, setLoginPassword] = useState("");
+
+    onAuthStateChanged(auth, (currentUser) = () => {
+        setUser(currentUser);
+    });
+
+    const register = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(
+                auth,
+                registerEmail,
+                registerPassword
+            );
+            console.log(user);
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    const login = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(
+              auth,
+              loginEmail,
+              loginPassword
+            );
+            console.log(user);
+        } 
+        catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    const logout = async () => {
+        await signOut(auth);
+    };
+
+
+    return (
+        <Box>
+            <Box>
+                <h1> Register User </h1>
+                <input
+                    placeholder="Email..."
+                    onChange={(event) => {
+                        setRegisterEmail(event.target.value);
+                    } } />
+
+                <input
+                    placeholder="Email..."
+                    onChange={(event) => {
+                        setRegisterPassword(event.target.value);
+                    } } />
+
+                <Button variant = "outlined" onClick={register}> Create User </Button>
+            </Box>
+        
+            <Box>
+                <h1> Login </h1>
+                <input
+                    placeholder="Email..."
+                    onChange={(event) => {
+                        setLoginEmail(event.target.value);
+                    } } />
+
+                <input
+                    placeholder="Email..."
+                    onChange={(event) => {
+                        setLoginPassword(event.target.value);
+                    } } />
+
+                <Button variant = "outlined" onClick={login}> Login </Button>
+            </Box>
+            
+            <Box>
+                <h2> Currently Loggin In As:</h2>
+                {user?.email}
+
+                <Button variant = "outlined" onClick={logout}> Sign Out </Button>
+            </Box>
+        </Box>
+    );
+}
+
+export default Login;
