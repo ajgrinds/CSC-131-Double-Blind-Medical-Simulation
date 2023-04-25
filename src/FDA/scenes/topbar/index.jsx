949ca7b5b1106612ffc506cc0c06b95314f73b23
parsabyt <124086692+parsabyt@users.noreply.github.com>
2023-04-25 00:19:30 -0700

    Topbar update(Search bar delete/Logout button)
    Signed-off-by: parsabyt <124086692+parsabyt@users.noreply.github.com>

--- src/FDA/scenes/topbar/index.jsx
+++ src/FDA/scenes/topbar/index.jsx
@@ -1,5 +1,5 @@
-import {Box, IconButton, useTheme} from '@mui/material';
-import { useContext } from 'react';
+import { Box, IconButton, Menu, MenuItem, Typography, useTheme } from '@mui/material';
+import { useContext, useState } from 'react';
 import { ColorModeContext, tokens } from '../../theme';
 import InputBase from '@mui/material/InputBase';
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
@@ -8,49 +8,66 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined"
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined"
 import SearchIcon from "@mui/icons-material/Search"
+import { Link } from 'react-router-dom';
 
 const TopBar = () => {
 
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
+    const [anchorEl, setAnchorEl] = useState(null);
 
-  return (
-    <Box display="flex" justifyContent="space-between" p={2}>
-        <Box
-            display="flex"
-            backgroundColor={colors.topbarColor[100]}
-            borderRadius="3px"
-        >
-            <InputBase sx={{ml: 2, flex: 2}} placeholder="Search"/>
-            <IconButton type="button" sx={{p: 1}}>
-                <SearchIcon/>
-            </IconButton>
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <Box display="flex" justifyContent="space-between" p={2}>
+            <Box
+                display="flex"
+                backgroundColor={colors.topbarColor[100]}
+                borderRadius="3px"
+            >
+            </Box>
+
+            <Box display="flex">
+                <IconButton onClick={colorMode.toggleColorMode}>
+                    {theme.palette.mode === 'dark' ?(
+                        <DarkModeOutlinedIcon/>
+                    ) : (
+                        <LightModeOutlinedIcon/>
+                    )}
+                    
+                </IconButton>
+                <IconButton>
+                    <NotificationsOutlinedIcon/>
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlinedIcon/>
+                </IconButton>
+                <IconButton
+                    onClick={handleClick}
+                >
+                    <PersonOutlinedIcon/>
+                </IconButton>
+                <Menu
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <Typography variant="body1">Logout</Typography>
+                        </Link>
+                    </MenuItem>
+                </Menu>
+            </Box>
         </Box>
-
-        <Box display="flex">
-            <IconButton onClick={colorMode.toggleColorMode}>
-                {theme.palette.mode === 'dark' ?(
-                    <DarkModeOutlinedIcon/>
-                ) : (
-                    <LightModeOutlinedIcon/>
-                )}
-                
-            </IconButton>
-            <IconButton>
-                <NotificationsOutlinedIcon/>
-            </IconButton>
-            <IconButton>
-                <SettingsOutlinedIcon/>
-            </IconButton>
-            <IconButton>
-                <PersonOutlinedIcon/>
-            </IconButton>
-
-        </Box>
-        
-    </Box>
-  );
+    );
 }
 
-export default TopBar
+export default TopBar;
