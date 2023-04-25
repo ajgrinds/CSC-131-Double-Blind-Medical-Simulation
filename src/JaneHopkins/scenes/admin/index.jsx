04e96ea9d1793182853bcf10498e44ec34f4b234
Laura Kay <laurakay@Lauras-MacBook-Pro.local>
2023-04-25 14:56:59 -0700

    Updated the admin page to track dosage and show eligible patients

--- src/JaneHopkins/scenes/admin/index.jsx
+++ src/JaneHopkins/scenes/admin/index.jsx
@@ -1,80 +1,146 @@
-import { useState } from "react";
-import {Box, Typography, useTheme, useMediaQuery} from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2";
-import { tokens } from "../../theme";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Button, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../../theme";
 import Header from "../../components/Header";
-import Button from '@mui/material/Button';
+import useJaneHopkins from "../../../vendiaHooks/useJaneHopkins";
 import { styled } from '@mui/material/styles';
 import { purple } from '@mui/material/colors';
 
 
-//import StatBox from "../../components/StatBox";
+const Patient = () =>{
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
 
-const Admin = () => {
+    const ColorButton = styled(Button)(({ theme }) => ({
+      color: theme.palette.getContrastText(purple[500]),
+      backgroundColor: colors.greenAccent[600],
+      '&:hover': {
+        backgroundColor: colors.greenAccent[500],
+      },
+    }));
 
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
+    const {entities} = useJaneHopkins();
+    const [patientList, setPatientList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    
 
-  const ColorButton = styled(Button)(({ theme }) => ({
-    color: theme.palette.getContrastText(purple[500]),
-    backgroundColor: colors.greenAccent[600],
-    '&:hover': {
-      backgroundColor: colors.greenAccent[500],
-    },
-  }));
+    useEffect(() => {
 
-  const smScreen = useMediaQuery(theme.breakpoints.up("sm"));
+        async function fetchData() {
 
-  return (
-    <Box m="20px">
+            try{
 
-        <Box 
-          display={smScreen ? "flex" : "block"}
-          flexDirection={smScreen ? "row" : "column"}
-          justifyContent={smScreen ? "space-between" : "start"}
-          alignItems={smScreen ? "center" : "start"}
-          m="10px 0"
-        >
-            <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+                const response = await entities.patient.list();
+                console.log(response);
+                setPatientList(response.items.map((patient, index) => ({
+                    ...patient,
+                    id: index + 1,
+
+                }))); 
+            } catch(error){
+                console.log(error);
+            } finally{
+                setIsLoading(false);
+            }
+        }
+
+        fetchData();
+        
+    }, [entities.patient]);
+
+    const columns = [
+
+        {   
+            field: "uuid", 
+            headerName: "UUID",
+            flex:1,           
+        }, 
+        {
+            field: "dob",
+            headerName: "DOB",
+            flex : 1,
+        }, 
+
+        {
+          field: "visits",
+          headerName: "Dose #",
+          flex : 1,
+          valueGetter: (params) => params.row.visits ? params.row.visits.length : 0,
+      }, 
+       
+        {
+            field: "view",
+            headerName: "Eligible Patient",
+            width: 150,
+            renderCell: (params) => (
+                
+                    <Button 
+
+                        variant="contained" 
+                        style={{ backgroundColor: colors.blueAccent[600]}} 
+                        
+                    > 
+                      ELIGIBLE
+                    </Button>
+                     
+            ),
+        },
+        
+    ];
+
+    return(
+        <Box m="20px">
+
+          <Box position="absolute" right="0" pr={{ xs: 1, md: 2 }} py={{ xs: 1, md: 2 }}>
+                <ColorButton variant="contained" size ='large' style={{ marginRight: "16px" }}>
+                   Notify FDA
+                </ColorButton>
+                <ColorButton variant="contained" size = 'large'>
+                    Send FDA Results
+                </ColorButton>
+            </Box>
+
+            <Header title="ADMINISTRATION" subtitle="Welcome to your dashboard"/>
+            <Box m={{ xs: '20px 0 0 0', md: '30px 0 0 0' }} height="75vh"
+
+              
+                sx={{
+                    "& .MuiDataGrid-root":{
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell":{
+                        borderBottom :"none",
+                    },
+                    "& .name-column--cell":{
+                        color: colors.greenAccent[300]
+                    },
+                    "& .MuiDataGrid-columnHeaders":{
+                        backgroundColor: colors.blueAccent[700],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller":{
+                        backgroundColor: colors.primary[400],
+                    },
+                    "& .MuiDataGrid-footerContainer":{
+                        borderTop: "none",
+                        backgroundColor: colors.blueAccent[700],
+                    }
+                }}
+            >
+                {isLoading ? (
+                    <Box>Loading...</Box>
+                ) : (
+                  <DataGrid rows={patientList.filter(patient => !patient.icdHealthCodes || patient.icdHealthCodes.length === 0)} columns={columns} />
+                )}
+
+                 
+                
+            </Box>
         </Box>
+    )
 
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}> 
-         
-          <Grid xs={12} sm={12} md={12} lg={4} xl={4}>
-            <Box
-              width="100%"
-              backgroundColor={colors.primary[400]}
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              sx={{height: 300}}
-            >
-              <Box sx={{ml: "30px"}}>
-
-                <Box sx={{display: "flex", alignContent: "center"}} mt="20px" ml="50px">
-                
-                </Box>
-
-              </Box>
-                
-            </Box>
-
-          </Grid>
-          <Grid xs={12} sm={12} md={12} lg={8} xl={8}>
-            <Box
-              width="100%"
-              backgroundColor={colors.primary[400]}
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              sx={{height: 300}}
-            >
-            
-            </Box>
-          </Grid>
-        </Grid>
-    </Box>
-  )
 }
 
-export default Admin;
+export default Patient;
