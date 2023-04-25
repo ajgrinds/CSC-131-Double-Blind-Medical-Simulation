1ddcaea4401ab647d3ad2dc9ff0079a44b02f954
parsabyt <124086692+parsabyt@users.noreply.github.com>
2023-04-25 15:51:22 -0700

    FDA.js Update
    Signed-off-by: parsabyt <124086692+parsabyt@users.noreply.github.com>

--- src/FDA/FDA.js
+++ src/FDA/FDA.js
@@ -1,38 +1,39 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import {Routes, Route, Navigate} from "react-router-dom";
+import {Routes, Route, Navigate, useLocation} from "react-router-dom";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 import TopBar from "./scenes/topbar";
-
 import FDADashboard from "./scenes/dashboard";
 import FDASidebar from "./scenes/sidebar";
 import FDAPatient from "./scenes/patient";
-
+import FDAStudies from "./scenes/studies";
+import "./scenes/smooth.css";
 
 function FDA() {
 
   const [theme, colorMode] = useMode();
+  const location = useLocation();
 
   return ( 
   <ColorModeContext.Provider value={colorMode}>
     <ThemeProvider theme={theme}>
       <CssBaseline/>
-      <div className="app">
+      <div style={{height: "100%", width: "100%"}} className="app">
 
        <FDASidebar/>
 
         <main className="content">
           <TopBar/>
-          
         
-          {/*Add all the routes for FDA here. i.e patients, assign drugs, etc */}
-          {/* only include the path name without /fda path preceding it such as... i.e ...  /patient  /assignDrug */}
-          {/* In App.js you will put the full path name. i.e. /fda/patient/  /fda/assignDrug */}
-          <Routes>
-            <Route path="/" element={<FDADashboard/>}/>
-            <Route path="/patient" element={<FDAPatient/>} />
-            
-          </Routes>
-
+          <TransitionGroup>
+            <CSSTransition key={location.key} classNames="smooth" timeout={300}>
+              <Routes location={location}>
+                <Route path="/" element={<FDADashboard/>}/>
+                <Route path="/patient" element={<FDAPatient/>}/>
+                <Route path="/studies" element={<FDAStudies/>}/>
+              </Routes>
+            </CSSTransition>
+          </TransitionGroup>
         </main>
 
       </div>
