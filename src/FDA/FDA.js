b0157c61bf23aaec0f1a3c89693788f37457e4c6
parsabyt <124086692+parsabyt@users.noreply.github.com>
2023-04-26 22:17:29 -0700

    Needed FDA updates
    Signed-off-by: parsabyt <124086692+parsabyt@users.noreply.github.com>

--- src/FDA/FDA.js
+++ src/FDA/FDA.js
@@ -1,18 +1,19 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import {Routes, Route} from "react-router-dom";
-
+import {TransitionGroup, CSSTransition} from "react-transition-group";
+import {Routes, Route, useLocation} from "react-router-dom";
 import { MyProSidebarProvider } from "./scenes/sidebar/MyProSideBar";
 import TopBar from "./scenes/topbar";
 
 import FDADashboard from "./scenes/dashboard";
-
+import FDAStudies from "./scenes/studies";
 import FDAPatient from "./scenes/patient";
-
+import "./scenes/smooth.css";
 
 function FDA() {
 
   const [theme, colorMode] = useMode();
+  const location = useLocation();
 
   return ( 
   <ColorModeContext.Provider value={colorMode}>
@@ -23,11 +24,15 @@ function FDA() {
         <div style={{height: "100%", width: "100%"}}>
           <TopBar/>
           <main>
-            <Routes>
-              <Route path="/" element={<FDADashboard/>}/>
-              <Route path="/patient" element={<FDAPatient/>} />
-              
-            </Routes>
+          <TransitionGroup>
+            <CSSTransition key={location.key} classNames="smooth" timeout={300}>
+              <Routes location={location}>
+                <Route path="/" element={<FDADashboard/>}/>
+                <Route path="/patient" element={<FDAPatient/>}/>
+                <Route path="/studies" element={<FDAStudies/>}/>
+              </Routes>
+            </CSSTransition>
+          </TransitionGroup>
 
           </main>
 
