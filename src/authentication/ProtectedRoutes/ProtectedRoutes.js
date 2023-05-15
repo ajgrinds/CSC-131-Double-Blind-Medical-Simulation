0e350385f249ab519886895d33054179074b2b54
Luke Simoni <luke.simoni2001@gmail.com>
2023-05-15 12:12:09 -0700

    Protected routes functionality

--- src/authentication/ProtectedRoutes/ProtectedRoutes.js
+++ src/authentication/ProtectedRoutes/ProtectedRoutes.js
@@ -1,14 +1,16 @@
 import {useLocation, Navigate, Outlet} from 'react-router-dom'
-import { UseAuth } from '../context/AuthContext'
+import { UseAuth, UseAuthType } from '../context/AuthContext'
 
-const ProtectedRoutes = (()) => {
+const ProtectedRoutes = ({allowedRoles}) => {
     const useAuth = UseAuth();
+    const location = useLocation();
+    const authRole = UseAuthType();
 
     return (
-        useAuth
+        allowedRoles?.includes(authRole)
         ? <Outlet /> 
         : <Navigate to = "/" state={{from:location}} replace/>
     );
 }
 
-export ProtectedRoutes;
+export default ProtectedRoutes;
